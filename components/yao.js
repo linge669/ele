ab5e4c9182bdcf40fcda4814cf993da68361269c
yaoxinjie <1072102644@qq.com>
2018-06-28 18:20:27 +0800

    配置文件Ok

---
+++ components/yao.js
@@ -0,0 +1,15 @@
+import React from "react";
+class Hasakei extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	
+	render(){
+		return(
+			<div>
+				<h1>哈撒尅</h1>
+			</div>
+		)
+	}
+}
+export default Hasakei
--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//地址要改
+import Hasakei from "./components/yao.js"
+
+
+//标签名改一哈
+ReactDOM.render(<Hasakei/>,document.getElementById("box"))
